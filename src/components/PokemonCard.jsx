945be5ddfc92711c91f138b3d552774899e93a12
ryanrasal <ryan.beaujot@outlook.fr>
2023-03-28 15:48:59 +0200

    finished challenge 2

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 const PokemonCard = () => {
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+  const [pokemon, setPokemon] = useState(pokemonList[1]);
+
   return (
     <div>
-      <figure>
-        <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-          alt=""
-        />
-        <figcaption>bulbasaur</figcaption>
-      </figure>
+      <p>{pokemon.name}</p>
+      {pokemon.imgSrc ? (
+        <img src={pokemon.imgSrc} alt={pokemon.name} />
+      ) : (
+        <p>???</p>
+      )}
     </div>
   );
 };
