698b153807c1b33b2e1c835e7898689e2f6a0036
ryanrasal <ryan.beaujot@outlook.fr>
2023-03-28 18:53:08 +0200

    finish

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
 const PokemonCard = ({ pokemonList }) => {
@@ -9,26 +9,37 @@ const PokemonCard = ({ pokemonList }) => {
     }).isRequired,
   };
 
+  useEffect(() => {
+    alert("hello pokemon trainer :");
+  },[]);
+  
   // UseState pour indexer pokemonList
   const [pokemonIndex, setPokemonIndex] = useState(0);
-// Bouton suivant 
+  // Bouton suivant
   const handleNext = () => {
-    pokemonIndex < pokemonList.length - 1 ?
-    setPokemonIndex(pokemonIndex + 1) : false
-  };
-// Bouton Précédent
-  const handlePrevious = () => {
-    pokemonIndex > 0 ?
-    setPokemonIndex(pokemonIndex - 1) : false
-  };
+      pokemonIndex < pokemonList.length - 1
+      ? setPokemonIndex(pokemonIndex + 1)
+      : false;
+    };
+    // Bouton Précédent
+    const handlePrevious = () => {
+        pokemonIndex > 0 ? setPokemonIndex(pokemonIndex - 1) : false;
+    };
+    
+    useEffect(() => {
+      pokemonIndex === 3 ? alert("pika pikachu !!!") : null;
+    }, [pokemonIndex]);
 
-  return (
-    <div>
+    return (
+        <div>
       <figure>
         <figcaption>
           <p>{pokemonList[pokemonIndex].name}</p>
         </figcaption>
-        <img src={pokemonList[pokemonIndex].imgSrc} alt={pokemonList[pokemonIndex].name} />
+        <img
+          src={pokemonList[pokemonIndex].imgSrc}
+          alt={pokemonList[pokemonIndex].name}
+        />
       </figure>
       <button onClick={() => handlePrevious()}>précédent</button>
       <button onClick={() => handleNext()}>suivant</button>
